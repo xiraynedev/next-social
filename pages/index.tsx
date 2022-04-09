ecfd344c1301f5f057c965476bb8c8debd5056dd
xiraynedev <xiraynedev@gmail.com>
2022-04-09 09:48:54 -0400

    initial commit with next.js setup

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className='text-red-500 text-3xl'>Welcome to Next Social!</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './components/**/*.{tsx,ts}',
+    './pages/**/*.{tsx,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
